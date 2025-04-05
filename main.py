a4dd7e9e84fa469114cfc559c4d82eaee07fbbef
Wajith <wawa@pop-os.localdomain>
2025-04-05 20:57:49 +0530

    limiting fps

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60)
 
 if __name__ == '__main__':
     main()
